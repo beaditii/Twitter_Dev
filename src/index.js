55d791bdd708aa933b366e758d5a549b26c3f1f7
Aditi Kumari <aditisingh49893@gmail.com>
2024-02-08 01:13:43 +0530

    Added codes

---
+++ src/index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const connect=require('./config/database');
+const app = express();
+
+const TweetRepository=require('./repository/tweet-repository');
+const Comment=require('./models/comment');
+//const Tweet=require('./models/tweet');
+app.listen(3000, async() => {
+    console.log('server started');
+    await connect();
+
+    console.log('Mongo db connected');
+
+    // const tweet=await Tweet.create({
+    //     content:'Fourth tweet',
+    //     userEmail:'a@b.com'
+    // });
+
+   // const tweets=await Tweet.find();
+   //const tweets=await Tweet.find({userEmail:'a@b.com'});
+
+//    const tweet=await Tweet.findById('65c3bdb914d42d671972d74c');
+//    tweet.userEmail = 'p@b.com';
+//    await tweet.save();
+
+   const tweetRepo=new TweetRepository();
+
+//    const tweet=await tweetRepo.create({
+//     content:'tweet with a comment'
+//    });
+//    console.log(tweet);
+//    tweet.comments.push({content:'first comment here'});
+//    await tweet.save();
+//    console.log(tweet);
+
+//  const tweet=await tweetRepo.create({content:'Tweet with comment schema'});
+//  const comment=await Comment.create({content:'new Comment'});
+//     console.log(tweet);
+//  tweet.comments.push(comment);
+//  await tweet.save();
+//    console.log(tweet);
+ 
+const tweet=await tweetRepo.getWithComment('65c3d9e998c6a17325e2c074');
+    console.log(tweet);
+
+});
