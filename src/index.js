12fd3d4159d0aaf3abcbc965a10e4a79e6eb5c53
Aditi Kumari <aditisingh49893@gmail.com>
2024-02-09 20:48:46 +0530

    Added controller and routes for create tweet api

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import {connect} from './config/database.js';
+import apiRoutes from './routes/index.js'
 const app = express();
 
  import TweetService from './services/tweet-service.js';
+
+ app.use(bodyParser.json());
+ app.use(bodyParser.urlencoded({ extended: true }));
+ app.use('/api',apiRoutes);
 
 app.listen(3000, async() => {
     console.log('server started');
     await connect();
 
     console.log('Mongo db connected');
-    let service=new TweetService();
-
-    await service.create({content:"#reMovEd tweet Schema"});
+   
    
 
 });
